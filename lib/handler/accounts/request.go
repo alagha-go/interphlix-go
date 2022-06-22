27212bdcb714492d7f703bdad99c89b4b1152223
alagha-go <abdihakimosman1480@gmail.com>
2022-06-22 18:58:06 +0000

    validate request

---
+++ lib/handler/accounts/request.go
@@ -0,0 +1,13 @@
+package accounts
+
+import "net/http"
+
+
+func ValidateRequest(req *http.Request) bool {
+	cookie, err := req.Cookie("token")
+	if err != nil {
+		return false
+	}
+	valid, _ := VerifyToken(cookie.Value)
+	return valid
+}
