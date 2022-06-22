322b2e13164bbef07bd9ab13ef20c2342faa5d38
alagha-go <abdihakimosman1480@gmail.com>
2022-06-22 18:21:13 +0000

    start http server

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"interphlix/lib/handler"
 	"interphlix/lib/variables"
+	"log"
+	"net/http"
+)
+
+var (
+	PORT = ":8000"
 )
 
 
 func main() {
 	variables.ConnectToDB()
+
+	err := http.ListenAndServe(PORT, handler.Router)
+	HandlError(err)
+}
+
+
+// handle errors by pannic
+func HandlError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
