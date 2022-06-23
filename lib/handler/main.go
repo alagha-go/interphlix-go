081f6622bf480adaa788757348bdfcee15d4e94e
alagha-go <abdihakimosman1480@gmail.com>
2022-06-23 16:28:38 +0000

    added web client

--- lib/handler/main.go
+++ lib/handler/main.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"interphlix/lib/handler/accounts"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
@@ -12,7 +13,12 @@ var (
 )
 
 func Main() {
+	fs := http.FileServer(http.Dir("./web/src/"))
 	// routes to work on account
 	Router.HandleFunc("/apis/sign-up", accounts.SignUp).Methods("POST")
 	Router.HandleFunc("/apis/myaccount", accounts.GetMyAccount).Methods("GET")
+
+
+	// server static files
+	Router.PathPrefix("/").Handler(fs)
 }
