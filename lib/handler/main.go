d204abf865bc34727cd4a11645c85094ea745526
alagha-go <abdihakimosman1480@gmail.com>
2022-07-02 06:49:26 +0000

    removed unneccessary routes

--- lib/handler/main.go
+++ lib/handler/main.go
@@ -13,15 +13,9 @@ var (
 )
 
 func Main() {
-	fs := http.FileServer(http.Dir("./web/src/"))
 	// routes to work on account
 	Router.HandleFunc("/apis/sign-up", accounts.SignUp).Methods("POST")
 	Router.HandleFunc("/apis/myaccount", accounts.GetMyAccount).Methods("GET")
-
-
-	// server static files
-	Router.HandleFunc("/favicon.ico", ServeFavicon).Methods("GET")
-	Router.PathPrefix("/").Handler(fs)
 }
 
 
