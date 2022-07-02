431b04a18a71af2dac48e2cfa15145e6b03d0750
alagha-go <abdihakimosman1480@gmail.com>
2022-07-02 18:56:40 +0000

    added endpoints to listen for login with ggogle

--- lib/handler/main.go
+++ lib/handler/main.go
@@ -14,7 +14,9 @@ var (
 func Main() {
 	// routes to work on account
 	Router.HandleFunc("/apis/sign-up", accounts.SignUp).Methods("POST")
-	Router.HandleFunc("/login", accounts.Login).Methods("POST")
+	Router.HandleFunc("/apis/login", accounts.Login).Methods("POST")
 	Router.HandleFunc("/apis/myaccount", accounts.GetMyAccount).Methods("GET")
 	Router.HandleFunc("/login/redirect", accounts.Redirect).Methods("GET")
+	Router.HandleFunc("/apis/sign-up/google", accounts.GoogleSignUp).Methods("GET")
+	Router.HandleFunc("/apis/login/google", accounts.GoogleLogin).Methods("GET")
 }
