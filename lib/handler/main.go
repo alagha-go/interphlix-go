30eb0fbfa2498b3d9cd3268845a2592091005b07
alagha-go <abdihakimosman1480@gmail.com>
2022-07-02 18:43:08 +0000

    added endpoint listeners

--- lib/handler/main.go
+++ lib/handler/main.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"interphlix/lib/handler/accounts"
-	"net/http"
 
 	"github.com/gorilla/mux"
 )
@@ -15,11 +14,7 @@ var (
 func Main() {
 	// routes to work on account
 	Router.HandleFunc("/apis/sign-up", accounts.SignUp).Methods("POST")
+	Router.HandleFunc("/login", accounts.Login).Methods("POST")
 	Router.HandleFunc("/apis/myaccount", accounts.GetMyAccount).Methods("GET")
-}
-
-
-func ServeFavicon(res http.ResponseWriter, req *http.Request) {
-	path := "./web/logo.ico"
-	http.ServeFile(res, req, path)
+	Router.HandleFunc("/login/redirect", accounts.Redirect).Methods("GET")
 }
