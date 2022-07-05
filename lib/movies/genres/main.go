61865e59110a589fb18e4cf57997d1ff8ee20c4a
alagha-go <abdihakimosman1480@gmail.com>
2022-07-05 16:19:39 +0000

    load genres to the local database

---
+++ lib/movies/genres/main.go
@@ -0,0 +1,31 @@
+package genres
+
+import (
+	"context"
+	"interphlix/lib/variables"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+
+func LoadGenres() {
+	var documents []interface{}
+	ctx := context.Background()
+	collection1 := variables.Client.Database("Interphlix").Collection("Genres")
+	collection := variables.Local.Database("Interphlix").Collection("Genres")
+
+	cursor, err := collection1.Find(ctx, bson.M{})
+	if err != nil {
+		log.Panic(err)
+	}
+	err = cursor.All(ctx, &documents)
+	if err != nil {
+		log.Panic(err)
+	}
+	_, err = collection.InsertMany(ctx, documents)
+	if err != nil && err != mongo.ErrEmptySlice {
+		log.Panic(err)
+	}
+}
