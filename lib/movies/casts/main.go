032c1393d77c8b7ddff1a9d1a5a723ec885a9920
alagha-go <abdihakimosman1480@gmail.com>
2022-07-07 17:35:53 +0000

    load casts

---
+++ lib/movies/casts/main.go
@@ -0,0 +1,38 @@
+package casts
+
+import (
+	"context"
+	"interphlix/lib/variables"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+
+func init() {
+	LoadCasts()
+}
+
+
+func LoadCasts() {
+	var documents []interface{}
+	ctx := context.Background()
+	collection1 := variables.Client.Database("Interphlix").Collection("Casts")
+	collection := variables.Local.Database("Interphlix").Collection("Casts")
+
+	cursor, err := collection1.Find(ctx, bson.M{})
+	HandleError(err)
+	err = cursor.All(ctx, &documents)
+	HandleError(err)
+	err = collection.Drop(ctx)
+	HandleError(err)
+	_, err = collection.InsertMany(ctx, documents)
+	HandleError(err)
+}
+
+func HandleError(err error) {
+	if err != nil && err != mongo.ErrEmptySlice {
+		log.Panic(err)
+	}
+}
