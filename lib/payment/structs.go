e0671440631e9be55f92da89de88b5278711d53a
alagha-go <abdihakimosman1480@gmail.com>
2022-07-14 17:26:03 +0000

    updated struct tags

--- lib/payment/structs.go
+++ lib/payment/structs.go
@@ -1,63 +1,65 @@
 package payment
 
+import "time"
+
 
 type Payment struct {
-	ID            					string 						`json:"id,omitempty" json:"id,omitempty"`
-	Txid          					string 						`json:"txid,omitempty" json:"txid,omitempty"`
-	ExplorerURL   					string 						`json:"explorer_url,omitempty" json:"explorer_url,omitempty"`
-	MerchantID    					string 						`json:"merchant_id,omitempty" json:"merchant_id,omitempty"`
-	Type          					string 						`json:"type,omitempty" json:"type,omitempty"`
-	CoinShortName 					string 						`json:"coin_short_name,omitempty" json:"coin_short_name,omitempty"`
-	WalletID      					string 						`json:"wallet_id,omitempty" json:"wallet_id,omitempty"`
-	WalletName    					string 						`json:"wallet_name,omitempty" json:"wallet_name,omitempty"`
-	Address       					string 						`json:"address,omitempty" json:"address,omitempty"`
-	Amount        					string 						`json:"amount,omitempty" json:"amount,omitempty"`
-	Confirmations 					int64  						`json:"confirmations,omitempty" json:"confirmations,omitempty"`
-	Date          					string 						`json:"date,omitempty" json:"date,omitempty"`
+	ID            					string 						`json:"id,omitempty" bson:"id,omitempty"`
+	Txid          					string 						`json:"txid,omitempty" bson:"txid,omitempty"`
+	ExplorerURL   					string 						`json:"explorer_url,omitempty" bson:"explorer_url,omitempty"`
+	MerchantID    					string 						`json:"merchant_id,omitempty" bson:"merchant_id,omitempty"`
+	Type          					string 						`json:"type,omitempty" bson:"type,omitempty"`
+	CoinShortName 					string 						`json:"coin_short_name,omitempty" bson:"coin_short_name,omitempty"`
+	WalletID      					string 						`json:"wallet_id,omitempty" bson:"wallet_id,omitempty"`
+	WalletName    					string 						`json:"wallet_name,omitempty" bson:"wallet_name,omitempty"`
+	Address       					string 						`json:"address,omitempty" bson:"address,omitempty"`
+	Amount        					string 						`json:"amount,omitempty" bson:"amount,omitempty"`
+	Confirmations 					int64  						`json:"confirmations,omitempty" bson:"confirmations,omitempty"`
+	Date          					time.Time 					`json:"date,omitempty" bson:"date,omitempty"`
 }
 
 type Invoice struct {
-	ID              				string           			`json:"id,omitempty" json:"id,omitempty"`
-	InvoiceID       				string           			`json:"invoice_id,omitempty" json:"invoice_id,omitempty"`
-	MerchantID      				string           			`json:"merchant_id,omitempty" json:"merchant_id,omitempty"`
-	URL             				string           			`json:"url,omitempty" json:"url,omitempty"`
-	TotalAmount     				Amount           			`json:"total_amount,omitempty" json:"total_amount,omitempty"`
-	PaidAmount      				Amount           			`json:"paid_amount,omitempty" json:"paid_amount,omitempty"`
-	UsdAmount       				string           			`json:"usd_amount,omitempty" json:"usd_amount,omitempty"`
-	ConversionRate  				ConversionRate   			`json:"conversion_rate,omitempty" json:"conversion_rate,omitempty"`
-	BaseCurrency    				string           			`json:"base_currency,omitempty" json:"base_currency,omitempty"`
-	Coin            				string           			`json:"coin,omitempty" json:"coin,omitempty"`
-	Name            				string           			`json:"name,omitempty" json:"name,omitempty"`
-	Description     				string           			`json:"description,omitempty" json:"description,omitempty"`
-	WalletName      				string           			`json:"wallet_name,omitempty" json:"wallet_name,omitempty"`
-	Address         				string           			`json:"address,omitempty" json:"address,omitempty"`
-	PaymentHistory  				[]PaymentHistory 			`json:"payment_history,omitempty" json:"payment_history,omitempty"`
-	Status          				string           			`json:"status,omitempty" json:"status,omitempty"`
-	StatusCode      				int64            			`json:"status_code,omitempty" json:"status_code,omitempty"`
-	NotifyURL       				string           			`json:"notify_url,omitempty" json:"notify_url,omitempty"`
-	SuceessURL      				string           			`json:"suceess_url,omitempty" json:"suceess_url,omitempty"`
-	FailURL         				string           			`json:"fail_url,omitempty" json:"fail_url,omitempty"`
-	ExpireOn        				string           			`json:"expire_on,omitempty" json:"expire_on,omitempty"`
-	InvoiceDate     				string           			`json:"invoice_date,omitempty" json:"invoice_date,omitempty"`
-	CustomData1     				string           			`json:"custom_data1,omitempty" json:"custom_data1,omitempty"`
-	CustomData2     				string           			`json:"custom_data2,omitempty" json:"custom_data2,omitempty"`
-	LastUpdatedDate 				string           			`json:"last_updated_date,omitempty" json:"last_updated_date,omitempty"`
+	ID              				string           			`json:"id,omitempty" bson:"id,omitempty"`
+	InvoiceID       				string           			`json:"invoice_id,omitempty" bson:"invoice_id,omitempty"`
+	MerchantID      				string           			`json:"merchant_id,omitempty" bson:"merchant_id,omitempty"`
+	URL             				string           			`json:"url,omitempty" bson:"url,omitempty"`
+	TotalAmount     				Amount           			`json:"total_amount,omitempty" bson:"total_amount,omitempty"`
+	PaidAmount      				Amount           			`json:"paid_amount,omitempty" bson:"paid_amount,omitempty"`
+	UsdAmount       				string           			`json:"usd_amount,omitempty" bson:"usd_amount,omitempty"`
+	ConversionRate  				ConversionRate   			`json:"conversion_rate,omitempty" bson:"conversion_rate,omitempty"`
+	BaseCurrency    				string           			`json:"base_currency,omitempty" bson:"base_currency,omitempty"`
+	Coin            				string           			`json:"coin,omitempty" bson:"coin,omitempty"`
+	Name            				string           			`json:"name,omitempty" bson:"name,omitempty"`
+	Description     				string           			`json:"description,omitempty" bson:"description,omitempty"`
+	WalletName      				string           			`json:"wallet_name,omitempty" bson:"wallet_name,omitempty"`
+	Address         				string           			`json:"address,omitempty" bson:"address,omitempty"`
+	PaymentHistory  				[]PaymentHistory 			`json:"payment_history,omitempty" bson:"payment_history,omitempty"`
+	Status          				string           			`json:"status,omitempty" bson:"status,omitempty"`
+	StatusCode      				int64            			`json:"status_code,omitempty" bson:"status_code,omitempty"`
+	NotifyURL       				string           			`json:"notify_url,omitempty" bson:"notify_url,omitempty"`
+	SuceessURL      				string           			`json:"suceess_url,omitempty" bson:"suceess_url,omitempty"`
+	FailURL         				string           			`json:"fail_url,omitempty" bson:"fail_url,omitempty"`
+	ExpireOn        				string           			`json:"expire_on,omitempty" bson:"expire_on,omitempty"`
+	InvoiceDate     				time.Time           		`json:"invoice_date,omitempty" bson:"invoice_date,omitempty"`
+	CustomData1     				string           			`json:"custom_data1,omitempty" bson:"custom_data1,omitempty"`
+	CustomData2     				string           			`json:"custom_data2,omitempty" bson:"custom_data2,omitempty"`
+	LastUpdatedDate 				time.Time           		`json:"last_updated_date,omitempty" bson:"last_updated_date,omitempty"`
 }
 
 type ConversionRate struct {
-	FromCurrency 						string 						`json:"from_currency,omitempty"`
-	ToCurrency 							string 						`json:"to_currency,omitempty"`
+	FromCurrency 					string 					`json:"from_currency,omitempty" bson:"from_currency,omitempty"`
+	ToCurrency 						string 					`json:"to_currency,omitempty" bson:"to_currency,omitempty"`
 }
 
 type Amount struct {
-	Coin 							string 						`json:"coin,omitempty"`
-	Currency 						string 						`json:"currency,omitempty"`
+	Coin 							string 						`json:"coin,omitempty" bson:"coin,omitempty"`
+	Currency 						string 						`json:"currency,omitempty" bson:"currency,omitempty"`
 }
 
 type PaymentHistory struct {
-	Txid         					string 						`json:"txid,omitempty"`
-	ExplorerURL  					string 						`json:"explorer_url,omitempty"`
-	Amount       					string 						`json:"amount,omitempty"`
-	Date         					string 						`json:"date,omitempty"`
-	Confirmation 					int64  						`json:"confirmation,omitempty"`
+	Txid         					string 						`json:"txid,omitempty" bson:"txid,omitempty"`
+	ExplorerURL  					string 						`json:"explorer_url,omitempty" bson:"explorer_url,omitempty"`
+	Amount       					string 						`json:"amount,omitempty" bson:"amount,omitempty"`
+	Date         					string 						`json:"date,omitempty" bson:"date,omitempty"`
+	Confirmation 					int64  						`json:"confirmation,omitempty" bson:"confirmation,omitempty"`
 }
